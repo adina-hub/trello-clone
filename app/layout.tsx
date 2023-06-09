efeefaf0d8d320e22a4297e25c41b88ba13bf25a
Adina Totorean <adina.totorean@hcl.com>
2023-06-09 18:45:59 +0300

    Added the 'Header' component.

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,7 +13,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body className='bg-[#F5F6F8]'>{children}</body>
     </html>
   )
 }
--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <main >
       {/* Header */}
-      
+      <Header />
       {/* Board */}
     </main>
   )
