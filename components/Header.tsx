4e6927af795569cfd5deba6b0a26eafce99a1d05
Adina Totorean <adina.totorean@hcl.com>
2023-06-14 14:26:40 +0300

    Implementing ChatGPT-4 functionality

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,16 +1,36 @@
 "use client"
 
 import Image from "next/image";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { MagnifyingGlassIcon, UserCircleIcon } from "@heroicons/react/24/solid";
 import Avatar from "react-avatar";
 import { useBoardStore } from "@/store/BoardStore";
+import fetchSuggestion from "@/lib/fetchSuggestion";
 
 function Header() {
-  const [searchString, setSearchString] = useBoardStore((state) => [
+  // const [loading, setLoading] = useState<boolean>(false)
+  // const [suggestion, setSuggestion] = useState<string>("")
+
+  const [board, searchString, setSearchString] = useBoardStore((state) => [
+    state.board,
     state.searchString,
     state.setSearchString
   ])
+  
+  {/* gpt functionality */}
+  // useEffect( () => {
+  //   if(board.columns.size === 0) return;
+  //   setLoading(true)
+
+  //   const fetchSuggestionFunc = async () => {
+  //     const suggestion = await fetchSuggestion(board)
+  //     setSuggestion(suggestion)
+  //     setLoading(false)
+  //   }
+
+  //   fetchSuggestionFunc();
+  // }, [board])
+
   return (
     <header>
       <div className="flex flex-col md:flex-row items-center p-5 bg-gray-500/10 
@@ -54,8 +74,13 @@ function Header() {
       <div className="flex items-center justify-center px-5 py-2 md:py-5">
         <p className="flex items-center p-5 text-sm font-light pr-5 shadow-xl
         rounded-xl w-fit bg-white italic max-w-3xl text-[#0055D1]">
-          <UserCircleIcon className="inline-block h-10 w-10 text-[#0055D1] 
-          mr-1"/>
+          <UserCircleIcon className={`inline-block h-10 w-10 text-[#0055D1] 
+          mr-1`}/>
+          {/* gpt functionality */}
+          {/* {suggestion && !loading 
+            ? suggestion 
+            : "GPT is summarising your tasks for the day..." 
+          } */}
           GPT is summarising your tasks for the day...
         </p>
       </div>
---
+++ lib/formatTodoForAI.ts
@@ -0,0 +1,19 @@
+const formatTodoForAI = (board: Board) => {
+  const todos = Array.from(board.columns.entries())
+
+  const flatArray = todos.reduce( (map, [key, value]) => {
+    map[key] = value.todos
+    return map
+  }, {} as { [key in TypedColumn]: Todo[] } )
+
+  const flatArrayCounted = Object.entries(flatArray).reduce(
+    (map, [key, value]) => {
+      map[key as TypedColumn] = value.length
+      return map
+    }, {} as { [key in TypedColumn]: number }
+  )
+
+  return flatArrayCounted
+}
+
+export default formatTodoForAI;
